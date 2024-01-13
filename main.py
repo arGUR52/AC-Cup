78a0429558f51b41e5fcb2919f74fab600b0c304
argur <arda.guerdal@tum.de>
2024-01-13 13:48:20 +0100

    u

---
+++ main.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+diet = pd.read_csv("diet.csv")
+recipes = pd.read_csv("recipes.csv")
+requests = pd.read_csv("requests.csv")
+reviews = pd.read_csv("reviews.csv")
+
+# B-b-b-b-b-b-bruuuuuuuuuuuuuhhhhhhhhhh!!!!!!!!
+
